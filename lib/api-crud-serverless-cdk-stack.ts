905d1ba606d843309576bf273e936b788ddb1d32
david-blg <david.stack.dev@gmail.com>
2024-07-13 12:59:18 +0200

    feat: Add Cognito User Pool creation in API CRUD serverless CDK stack

--- lib/api-crud-serverless-cdk-stack.ts
+++ lib/api-crud-serverless-cdk-stack.ts
@@ -2,12 +2,22 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { createApiGateway } from './api/createApiGateway';
 import { createLambdaGetNotes } from './lambdas/create_notes/construct';
+import { createCognitoIAM } from './auth/createCognitoIAM';
 
 
 export class ApiCrudServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
+
+    // Create Cognito Pool 
+    const cognitoPool = createCognitoIAM(this, {
+      userPoolName: 'Cognito-User-Pool',
+      domainPrefix: 'api-crud-serverless-demo'
+    })
+
+    // Create a Lambda function
     const lamdaGetNotes = createLambdaGetNotes(this, {
       functionName: 'Lambda-Get-Notes',
       description: 'This Lambda function will return all notes'
---
+++ lib/auth/createCognitoIAM.ts
@@ -0,0 +1,65 @@
+import { AccountRecovery, OAuthScope, UserPool } from "aws-cdk-lib/aws-cognito"
+import { Construct } from "constructs"
+
+interface CognitoIAMProps {
+    userPoolName: string
+    domainPrefix: string
+}
+
+export const createCognitoIAM = (scope: Construct, props: CognitoIAMProps) => {
+    const { userPoolName, domainPrefix } = props
+
+    // Create a Cognito User Pool
+    const userPool = new UserPool(scope, userPoolName, {
+        userPoolName,
+        smsRole: undefined,
+        selfSignUpEnabled: true,
+        signInAliases: {
+            email: true
+        },
+        autoVerify: {
+            email: true
+        },
+        standardAttributes: {
+            email: {
+                required: true,
+                mutable: true
+            }
+        },
+        passwordPolicy: {
+            minLength: 8,
+            requireLowercase: true,
+            requireDigits: true,
+            requireSymbols: true,
+            requireUppercase: true
+        },
+        accountRecovery: AccountRecovery.EMAIL_ONLY,
+    })
+
+    // Create a Cognito User Pool Client 
+    userPool.addClient('UserPoolClient', {
+        userPoolClientName: 'UserPoolClient',
+        generateSecret: false,
+        authFlows: {
+            // userPassword: true,
+            userSrp: true
+        },
+        oAuth: {
+            flows: {
+                authorizationCodeGrant: true,
+            },
+            scopes: [
+                OAuthScope.OPENID,
+            ],
+        }
+    })
+
+    // Create a Cognito User Pool Domain
+    userPool.addDomain('UserPoolDomain', {
+        cognitoDomain: {
+            domainPrefix,
+        }
+    })
+
+    return userPool
+}
